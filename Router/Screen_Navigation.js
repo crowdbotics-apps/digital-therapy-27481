af33d79f6ef7af8abfedadc3beb1765172c978fa
Talhaasif95 <Engineertalha@yahoo.com>
2021-09-17 19:58:06 +0500

    Item 4,5,6 and 9 done

--- Router/Screen_Navigation.js
+++ Router/Screen_Navigation.js
@@ -6,11 +6,6 @@ import { createStackNavigator } from "@react-navigation/stack"
 import SplashScreen from "../screens/Onboarding/SplashScreen"
 import LoginScreen from "../screens/Onboarding/LoginScreen"
 import SignupScreen from "../screens/Onboarding/SignupScreen"
-import OtpScreen from "../screens/Onboarding/OtpScreen"
-import ForgotScreen from "../screens/Onboarding/ForgotPassword"
-import RecoverPassword from "../screens/Onboarding/RecoverPassword"
-import HomeScreen from "../screens/Dashboard/HomeScreen"
-import ProfileScreen from "../screens/Dashboard/ProfileScreen"
 
 const Stack = createStackNavigator()
 
@@ -34,31 +29,6 @@ function App() {
         options={{ headerShown: false }}
         name="SignupScreen"
         component={SignupScreen}
-      />
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="OTPScreen"
-        component={OtpScreen}
-      />
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="ForgotScreen"
-        component={ForgotScreen}
-      />
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="RecoverPassword"
-        component={RecoverPassword}
-      />
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="HomeScreen"
-        component={HomeScreen}
-      />
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="ProfileScreen"
-        component={ProfileScreen}
       />
     </Stack.Navigator>
   )
