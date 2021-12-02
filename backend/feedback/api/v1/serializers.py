8296543c0ef39db727eaaec16ebec0bb13d8775f
p8ul <pkinuthia10@gmail.com>
2021-12-02 17:19:09 +0300

    remove user id when creating feedback

--- backend/feedback/api/v1/serializers.py
+++ backend/feedback/api/v1/serializers.py
@@ -4,6 +4,20 @@ from feedback.models import Feedback
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField(required=False)
+
     class Meta:
         model = Feedback
         fields = '__all__'
+
+    def get_user(self, instance):
+        return {
+            "id": instance.user.id,
+            "fullname": instance.user.fullname()
+        }
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+
+        instance = Feedback(user=user, **validated_data)
+        return instance
