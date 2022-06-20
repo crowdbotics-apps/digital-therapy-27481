595bc7e6f558a5b211d124bb7514eee4a2efe8b1
p8ul <pkinuthia10@gmail.com>
2022-06-20 19:08:22 +0300

    pagination limit to 200

--- backend/digital_therapy_27481/settings.py
+++ backend/digital_therapy_27481/settings.py
@@ -195,7 +195,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 25
+    'PAGE_SIZE': 200
 }
 
 # Custom user model
